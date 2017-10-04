7fcad4d5ef57761cd4e2117ee5b0d3ed771a55bd
Lhoir Simon <DarkChyper@users.noreply.github.com>
2017-10-04 08:40:55 +0200

    Update HomeController.php

--- src/AppBundle/Controller/HomeController.php
+++ src/AppBundle/Controller/HomeController.php
@@ -17,6 +17,7 @@ class HomeController extends Controller
 {
     /**
      * @Route("/", name="homepage")
+     * @Method("GET")
      */
     public function indexAction(Request $request, SessionService $sessionService)
     {
